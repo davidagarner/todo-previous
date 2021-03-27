2f558ced5af41726891d845b014c47c531b2b6aa
Davida Garner <davidac.garner@gmail.com>
2021-03-27 19:49:08 -0400

    second commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://davidag:resilient@cluster0.zmptf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'dbName': 'demo'
+};
